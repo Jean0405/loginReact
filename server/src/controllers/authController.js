1e4480165caf3e14b4a1055bd53dff8fbf4c1e3a
Jean0405 <jeanpierrea0405@gmail.com>
2023-09-02 16:03:14 -0500

    feat: :sparkles: Se crean módulos de login
    estos módulos realizan las peticiones a la db para verificar la existencia del usuario

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,10 +1,9 @@
 import * as authServices from "../services/authService.js";
 
-
-export const login = async (req, res)=>{
+export const login = async (req, res) => {
   try {
     let data = await authServices.login(req.body);
-    res.status(200).json({status:200, message: "successful login ", data: data})
+    res.status(200).json({ status: 200, message: "successful ", data: data });
   } catch (error) {
     res.status(500).send({
       status: 500,
@@ -14,4 +13,4 @@ export const login = async (req, res)=>{
       },
     });
   }
-}
+};
--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,11 +1,14 @@
-import {connDB} from "../databases/connectDB.js"
+import { connDB } from "../databases/connectDB.js";
 
-export const login = async(info)=>{
-let db = await connDB();
-let collection = db.collection("users");
-let resultado = await collection.find({email: info.email, password: info.password}).toArray();
+//LOG IN
+export const login = async (info) => {
+  let db = await connDB();
+  let collection = db.collection("users");
+  let resultado = await collection
+    .find({ email: info.email, password: info.password })
+    .toArray();
 
-if(!resultado) return res.status(404).json("unregistered user");
+  return resultado;
+};
 
-return resultado;
-}
+//REGISTER
