06a931224284d70b6bd37f85cf7235a85db755b5
Jean0405 <jeanpierrea0405@gmail.com>
2023-09-03 12:07:37 -0500

    feat: :sparkles: Se agregan rutas de paginas
    Se agregaron las rutas para el homepage al archivo APP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./components/SignIn";
+import {SignIn} from "./components/SignIn";
 import { SignUp } from "./components/SignUp";
+import { Home } from "./components/Home";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
