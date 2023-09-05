3cee43cea790faf0fe5649133671fc34a0291a2c
Jean0405 <jeanpierrea0405@gmail.com>
2023-09-04 22:28:30 -0500

    feat: :sparkles: se crean los validadores
    estos validadores se encargan de verificar la estructura y tipo de datos que entran desde el body

--- server/src/databases/connectDB.js
+++ server/src/databases/connectDB.js
@@ -2,11 +2,9 @@ import "dotenv/config";
 import { database } from "../configurations/serverConfig.js";
 import { MongoClient } from "mongodb";
 
-
 const URI = `mongodb+srv://${database.USER}:${database.PASSWORD}@practica.4b4nkjj.mongodb.net/${database.DB}`;
 
 export async function connDB() {
-  console.log(database.USER);
   const client = new MongoClient(URI);
   try {
     await client.connect();
--- server/src/v1/routes/authRoute.js
+++ server/src/v1/routes/authRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as authControllers from "../../controllers/authController.js";
+import { registerSchema } from "../../models/authSchemas.js";
 
 const v1Auth = Router();
 
 v1Auth
   .all("/login", authControllers.login)
-  .post("/signUp", authControllers.signUp);
+  .post("/signUp", registerSchema, authControllers.signUp);
 
 export default v1Auth;
